15b0763fbf045e731768dc01120b3f44aa764fcf
Wellington Rafael <wellington.copt@gmail.com>
2020-12-17 18:32:12 -0400

    Implementado funcao de somar

---
+++ src/js/calculadora.js
@@ -0,0 +1,25 @@
+var Calculadora = {
+        adicionar: function(num1, num2) {
+            if (isNaN(num1) || isNaN(num2))
+                return 0;
+
+            num1 = parseFloat(num1);
+            num2 = parseFloat(num2);
+            return num1 + num2
+        },
+        subtrair: function(num1, num2) {
+            return 0
+        },
+        dividir: function(num1, num2) {
+            return 0
+        },
+        multiplicar: function(num1, num2) {
+            return 0
+        },
+};
+
+//usado na função 'require do NodeJS'
+if (typeof module !== 'undefined'
+    && typeof module.exports !== 'undefined'){
+    module.exports = Calculadora;
+};
